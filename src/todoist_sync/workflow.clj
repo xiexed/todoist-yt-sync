b921878f959f34af1b18287d4d830799e8f98e19
xiexed <xiexed@gmail.com>
2020-12-26 21:33:36 +0300

    adding custom tags

--- src/todoist_sync/workflow.clj
+++ src/todoist_sync/workflow.clj
@@ -4,32 +4,43 @@
             [clojure.data.json :as json]
             [clojure.string :as str]))
 
-(def scheduled-tag-id "68-204271")
+(defn to-id-and-summary [resp]
+  {:issue   (or (:idReadable resp) (str (get-in resp [:project :shortName]) "-" (:numberInProject resp)))
+   :summary (:summary resp)})
 
-(defn add-scheduled-tag-for-issues [yt-token issues]
-  (doseq [issue issues
-          :let [tags-query-response (yt-client/issue {:key yt-token} issue {:fields "tags(name,id)"} {:throw-exceptions false})]
-          :when (not (:error tags-query-response))
-          :let [tags (:tags tags-query-response)]
-          :when (not-any? #(= scheduled-tag-id (:id %)) tags)
-          :let [new-tags {:tags (conj tags {:id scheduled-tag-id})}]]
-    (println "issue" issue "tags" tags "filter" new-tags)
-    (yt-client/yt-request {:key yt-token} :post (str "issues/" issue) {:query-params {:fields "tags"} :body (json/write-str new-tags)})))
+(defn add-tag-for-issues [yt-token issues tag-name]
+  (let [issues (not-empty
+                 (for [issue issues
+                       :let [tags-query-response
+                             (yt-client/issue {:key yt-token}
+                                              issue {:fields "id,idReadable,summary,tags(name,id)"}
+                                              {:throw-exceptions false})]
+                       :when (not (:error tags-query-response))
+                       :let [tags (:tags tags-query-response)]
+                       :when (not-any? #(= tag-name (:name %)) tags)]
+                   tags-query-response))]
+    (when (not-empty issues)
+      (yt-client/yt-request {:key yt-token}
+                            :post "commands"
+                            {:body (json/write-str {:issues issues
+                                                    :query  (str "add tag: " tag-name)})}))
+    (map to-id-and-summary issues)))
 
-(defn get-all-scheduled-issues [yt-token]
-  (->> (yt-client/issues {:key yt-token} "assigned to: me tag: in-my-plan")
-       (map (fn [resp] {:issue (str (get-in resp [:project :shortName]) "-" (:numberInProject resp)) :summary (:summary resp)}))))
+(defn get-all-tagged-issues [yt-token tag-name]
+  (->> (yt-client/issues {:key yt-token} (str "tag: " tag-name))
+       (map to-id-and-summary)))
 
 (defn update-scheduled-tag [{yt-token :youtrack} body]
   (let [issues (thd/extract-issues-from-html (:text body))
-        _ (add-scheduled-tag-for-issues yt-token issues)
-        known-scheduled-issues (get-all-scheduled-issues yt-token)]
-    {:issues-added   issues
+        tag-name (:tag (:settings body) "in-my-plan")
+        added (add-tag-for-issues yt-token issues tag-name)
+        known-scheduled-issues (get-all-tagged-issues yt-token tag-name)]
+    {:issues-added   added
      :issues-missing (let [issues-set (set issues)]
                        (remove #(issues-set (:issue %)) known-scheduled-issues))}))
 
 (def handlers
-  [{:name      "Update Scheduled Tag"
+  [{:name      "Sync tag for Issues"
     :available #(:youtrack %)
     :handler   update-scheduled-tag}
    {:name      "Convert Html to Markdown"
