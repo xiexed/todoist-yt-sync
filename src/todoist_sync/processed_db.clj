74bfdb924a44fc442a9e0be26ce7cda6765d8f87
xiexed <xiexed@gmail.com>
2021-03-14 20:34:39 +0300

    kinda postgress working on wildfly

--- src/todoist_sync/processed_db.clj
+++ src/todoist_sync/processed_db.clj
@@ -4,20 +4,24 @@
             [honeysql.helpers :refer :all :as helpers]
             [java-time :as t])
   (:import (com.typesafe.config ConfigFactory)
-           (java.time LocalDateTime)))
+           (java.time LocalDateTime)
+           (javax.naming InitialContext)))
 
 (require '[next.jdbc :as jdbc])
 
-(def ds (let [conf (ConfigFactory/load)]
-          (jdbc/get-datasource {:dbtype   "postgresql"
-                                :dbname   "todoist-sync"
-                                :user     (.getString conf "pg.user")
-                                :password (.getString conf "pg.password")})))
+(def ds (delay (let [ds (or
+                          (try (.lookup (InitialContext.) "java:jboss/datasources/todoist-sync-pg") (catch Exception e (println e) nil))
+                          (let [conf (ConfigFactory/load)]
+                            (jdbc/get-datasource {:dbtype   "postgresql"
+                                                  :dbname   "todoist-sync"
+                                                  :user     (.getString conf "pg.user")
+                                                  :password (.getString conf "pg.password")})))]
+                 (jdbc/execute! ds [(slurp (io/resource "create_db.sql"))])
+                 ds)))
 
-(jdbc/execute! ds [(slurp (io/resource "create_db.sql"))])
 
 (defn store-tag-sync-query [user tag html]
-  (jdbc/execute! ds 
-                 (-> (insert-into :tag_syncs) 
-                     (values [{:yt_user_id user :date (LocalDateTime/now) :tag tag :html html}]) 
+  (jdbc/execute! @ds
+                 (-> (insert-into :tag_syncs)
+                     (values [{:yt_user_id user :date (LocalDateTime/now) :tag tag :html html}])
                      (sql/format))))
