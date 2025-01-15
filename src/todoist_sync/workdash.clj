8d914777cf51a80095c2399f4c34a8142dbaf7f0
Nicolay.Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2025-01-15 20:57:24 +0100

    loading more info

--- src/todoist_sync/workdash.clj
+++ src/todoist_sync/workdash.clj
@@ -2,20 +2,18 @@
   (:require [todoist-sync.yt-client :as yt-client]
             [clojure.string :as s]))
 
-(def dashboards
-  [{:id "IDEA-A-2100662318" :assignee "Nicolay Mitropolsky"}
-   {:id "IDEA-A-2100662338" :assignee "Polina Popova"}
-   {:id "IDEA-A-2100662330" :assignee "Not team members"}
-   {:id "IDEA-A-2100662325" :assignee "Yuri Trukhin"}
-   {:id "IDEA-A-2100662319" :assignee "Andrei Ogurtsov"}
-   {:id "IDEA-A-2100662321" :assignee "Vladimir Petrenko"}
-   {:id "IDEA-A-2100662323" :assignee "Andrei Beliaev"}
-   {:id "IDEA-A-2100662320" :assignee "Petr Galunov"}
-   {:id "IDEA-A-2100662322" :assignee "Nurullokhon Gulomkodirov"}
-   {:id "IDEA-A-2100662324" :assignee "Nikita Katkov"}])
+(defn load-dashboard-articles
+  ([yt-token article]
+   (yt-client/get-from-yt {:key yt-token} (str "/articles/" article "/childArticles") {:fields "id,summary,idReadable"}))
+  ([yt-token]
+   (load-dashboard-articles yt-token "IDEA-A-2100662287")))
 
 (defn load-article [yt-token article]
   (yt-client/get-from-yt {:key yt-token} (str "/articles/" article) {:fields "content,summary"}))
+
+(defn load-mentioned-in [yt-token article]
+  (->> (yt-client/issues {:key yt-token} (str "mentioned in: " article) {:fields "id,idReadable"})
+       (map #(select-keys % [:idReadable :id]))))
 
 (defn parse-md-to-sections [text]
   (->> (s/split-lines text)
@@ -31,7 +29,7 @@
                      (let [issue-id (first (s/split trimmed-line #"\s+"))]
                        (if (empty? acc)
                          (proccor [{:header nil
-                                   :issues []}] line)
+                                    :issues []}] line)
                          (update-in acc [(dec (count acc)) :issues] conj issue-id)))
 
                      :else acc)))
@@ -44,22 +42,36 @@
     (keep (fn [[k v]] (when (> v 1) k)) frequencies)))
 
 
+(defn- custom-field [issue-data name]
+  (->> (:customFields issue-data)
+       (filter #(= name (:name %)))
+       (first)
+       :value))
+
 ; (map #(wd/load-issue-data my-yt-token %) (mapcat :issues (wd/parse-md-to-sections text)))
 (defn load-issue-data [yt-token issue]
   ;(map (fn [issue] (filter (fn [e] (#{"Assignee" "Planned for"} (:name e))) (yt-client/get-from-yt {:key yt-token} (str "issues/" issue "/customFields") {:fields "id,name,value(name, value, id)"}))))
-  (let [custom-fields (yt-client/get-from-yt {:key yt-token} (str "issues/" issue "/customFields") {:fields "id,name,value(name, value, id)"})
-        selected_fields (filter (fn [e] (#{"Assignee" "Planned for"} (:name e))) custom-fields)
-        assignee-field (first (filter #(= "Assignee" (:name %)) selected_fields))
-        planned-for-field (first (filter #(= "Planned for" (:name %)) selected_fields))]
-    {:issue       issue
-     :assignee    (get-in assignee-field [:value :name])
-     :planned-for (map :name (:value planned-for-field))}))
+  (let [issue-data (yt-client/get-from-yt {:key yt-token}
+                                          (str "issues/" issue)
+                                          {:fields "id,idReadable,resolved,customFields(id,name,value(name, value, id))"})]
+    {:idReadable  (:idReadable issue-data)
+     :id          (:id issue-data)
+     :resolved    (:resolved issue-data)
+     :assignee    (:name (custom-field issue-data "Assignee"))
+     :state       (:name (custom-field issue-data "State"))
+     :planned-for (map :name (custom-field issue-data "Planned for"))}))
 
 (defn report1 [yt-token]
-  (map (fn [d] (let [loaded (load-article yt-token (:id d))
-                     _ (println "id" (:id d))]
-                 {:assignee (:summary loaded)
-                  :issues   (parse-md-to-sections (:content loaded))})) dashboards))
+  (->> (load-dashboard-articles yt-token)
+       (pmap (fn [d]
+               (let [loaded (load-article yt-token (:id d))]
+                 {:assignee     (:summary loaded)
+                  :idReadable   (:idReadable d)
+                  :issues       (->> (parse-md-to-sections (:content loaded))
+                                     (map (fn [entry]
+                                            (update-in entry [:issues] (fn [issues]
+                                                                         (map #(load-issue-data yt-token %) issues))))))
+                  :mentioned-in (load-mentioned-in yt-token (:idReadable d))})))))
 
 (defn report2 [yt-token report1-result]
 
